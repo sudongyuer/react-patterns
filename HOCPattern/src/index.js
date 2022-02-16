c170f7b9f547228e9f4fd35599d4bc4f71b99964
sudongyu <976499226@qq.com>
2022-02-16 22:41:59 +0800

    feat: add HOC pattern project

---
+++ HOCPattern/src/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { render } from "react-dom";
+
+import DogImages from "./DogImages";
+import "./styles.css";
+ 
+function App() {
+  return (
+    <div className="App">
+      <h1>
+        Browse Dog Images{" "}
+        <span role="img" aria-label="emoji">
+          🐕
+        </span>
+      </h1>
+      <DogImages />
+    </div>
+  );
+}
+
+render(<App />, document.getElementById("root"));
---
+++ HOCPattern/src/withLoader.js
@@ -0,0 +1,23 @@
+import React, { useEffect, useState } from "react";
+
+export default function withLoader(Element, url) {
+  return (props) => {
+    const [data, setData] = useState(null);
+
+    useEffect(() => {
+      async function getData() {
+        const res = await fetch(url);
+        const data = await res.json();
+        setData(data);
+      }
+
+      getData();
+    }, []);
+
+    if (!data) {
+      return <div>Loading...</div>;
+    }
+
+    return <Element {...props} data={data} />;
+  };
+}
